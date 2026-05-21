413a7e7988b1a85d6c978cd4c2fd45e84af7fd4b
Ciel Baumann <ciel.baumann@gmail.com>
2026-05-22 00:31:21 +1200

    Top-level edits

--- detect_communities/pipeline.py
+++ detect_communities/pipeline.py
@@ -14,28 +14,36 @@ from queue import Queue
 type DDF = ddf.DataFrame
 
 
-def run(connectome: DDF) -> DDF:
+def run(connectome: DDF, args) -> DDF:
     """ Submit component jobs then tag connectome with community IDs """
     client = get_client()
     queue, communities = Queue(), []
-    initial_components = graph_utils.get_components(connectome)
+    initial_components = client.submit(graph_utils.get_components, connectome).result()
     for component in initial_components:
         queue.put(component)
     
     # Iterate until all connectome communities are found
     while not queue.empty():
         component = queue.get()
-        f = client.submit(modified_girvan_newman, component)
+        f = client.submit(modified_girvan_newman, component, args.madk)
         new_components = f.result() # List of tuples (ddf, bool)
         
         for df, should_continue in new_components:
+            size_f = client.submit(count_edges, df)
+            size = size_f.result()
+            if size < args.minsize:
+                continue # Skip too-small components
             if should_continue:
-                queue.put(cleaned[0])
+                queue.put(df)
             elif df:
                 communities.append(df)
                 
     tagged = tag_edges(connectome, communities)
     return tagged
+
+
+def count_edges(df):
+    return df.map_partitions(len).sum().compute()
 
 
 def tag_edges(connectome: DDF, communities: list[DDF]) -> DDF:
@@ -50,32 +58,31 @@ def tag_edges(connectome: DDF, communities: list[DDF]) -> DDF:
     for community, community_id in zip(communities, community_ids):
         community["community_id"] = community_id
     
-    big_community_df = ddf.concat(community_dfs)
+    big_community_df = ddf.concat(communities).persist()
     tagged = connectome.merge(big_community_df, 
                               on = ["pre", "post"], 
                               how="left")
-    tagged = tagged.sort_values(["neuropil", "community_id"])
+    tagged = tagged.sort_values(["neuropil", "community_id"]).persist()
     return tagged
 
 
-def modified_girvan_newman(component: DDF) -> list[tuple[DDF,bool]]:
+def modified_girvan_newman(component: DDF, k: float) -> list[tuple[DDF,bool]]:
     """ Remove bridge edges from component dataframe.
+    
+    k is used in get_upper_threshold() as a multiplier.
     
     Returns a list of tuples containing component dataframes and a boolean for
     whether processing should continue. 
     
     If bool continue is True, the caller should apply another round of modified
     girvan newman on each of the returned component dataframes. continue = False
-    occurs when no edges can be removed from the component (community found), or
-    when the component is too small, in which case None is returned in place of
-    the dataframe.
+    occurs when no edges can be removed from the component (community found).
     
     """
     component = graph_utils.prune(component)
-    # Return None if component too small - how to do without compute?
     
     edge_scores = edge_scoring.get_edge_scores(component)
-    upper_score_threshold = edge_scoring.get_upper_threshold(edge_scores)
+    upper_score_threshold = edge_scoring.get_upper_threshold(edge_scores, k)
     new_df, num_chopped = edge_scoring.chop(
         component, edge_scores, upper_score_threshold)
     
